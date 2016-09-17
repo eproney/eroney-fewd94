1d3ed1b0b008077e93b9fea0eb73320ef59690b2
eproney <eproney@gmail.com>
2016-09-17 13:31:02 -0400

    Finished Relaxr Homework

---
+++ Class07/first-javascript/script.js
@@ -0,0 +1,21 @@
+alert("Hello");
+
+document.write('heloooooo');
+
+document.write('<br>');
+
+  var num1 = 10 * 10;
+
+document.write(num1);
+
+console.log(num1);
+
+// Function
+
+function doSomething(){
+    console.log('this is working');
+}
+
+// Call the Function
+
+doSomething();
